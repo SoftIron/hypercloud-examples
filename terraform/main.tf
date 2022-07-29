27de39da21792f4b7961d9e6fe0057e97f969282
Pedro Alvarez <pedro.alvarez@softiron.com>
2022-07-29 09:28:19 +0100

    Abstract some values into variables

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,12 @@
 module "controllers" {
   source          = "./modules/tf-module-hypercloud-node"
   group_name      = "SoftIron PE"
-  num             = 3
+  num             = var.k3s_controllers
   name            = "k3s_controller"
   cpus            = 1
   vcpus           = 2
   memory          = 1024
-  ssh_key         = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC1jman9yJ/cjGZtsr7m+QR3JTd+ZikIfbrNA+tqUbo0x/yFvw0VIF7PFe99pFpzlfNqjqk6/u61g4pQSPDnBHT8IxN43V0K97ul7hLag0+AWP2/qyvxW4yTX7sXr9KOCwa9jaOs2VCGr3pZf0EKulh0NW8ZFwBxh9XjhJGl+k4Witp9k4jTB6xdwVHQaFPP8mcDYcTyWSioPDiLyfml5+Dg9T1OoRWFil+OBZpMx0fOc5K2ZNXo7vIXS2aqeLSNOoHQJ52EhcVzAgwBPhcuQVjyrVCtw4m2zMfjdtTVQ78hpitfBfUgcIOCPZ75Tmx3dj40OV6HtxWYHDgO7427SY9GfnsfhajmtS0QGcsfFPA4jdTdiZly9WOnJr5JZb6PW3PdNZR5I33JkVBLNbg1hFdQqs6dTjL9LG99UUYtC5S0lSuklpwyQCIt5W95n/9JRB2jPv/eg7xQ2htrRm1PFiC2zVJ/l7DWc03cws747mOvMXq9ETOjMJI9DMtTzwsNrc= pedroalvarez@ironx1"
+  ssh_key         = var.k3s_ssh-key
   image_id        = 26
   network_id      = 2
   security_groups = [0, 101]
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,2 +1,7 @@
 variable "one_username" {}
 variable "one_password" {}
+
+variable "k3s_loadbalancers" {}
+variable "k3s_controllers" {}
+variable "k3s_workers" {}
+variable "k3s_ssh-key" {}
