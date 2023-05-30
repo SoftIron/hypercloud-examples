66515112dc5407ffd73cc78ac3f802580e771115
howels <howels@users.noreply.github.com>
2023-05-30 13:35:05 +0100

    Revert unneeded changes

--- tf-modules/tf-module-hypercloud-node/main.tf
+++ tf-modules/tf-module-hypercloud-node/main.tf
@@ -58,6 +58,4 @@ resource "opennebula_virtual_machine" "instance" {
       security_groups = nic.value["net_sgs"]
     }
   }
-
-  sched_requirements = "(ARCH = \"x86_64\") & (HYPERVISOR = \"kvm\")"
 }
